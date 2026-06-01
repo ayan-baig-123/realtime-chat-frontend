fdc5e2d98ce3b28e917d1729402ff32f0e35a22d
Ayan Baig <ayanbaig@iak.ngo>
2026-06-01 11:52:37 +0500

    Change WebSocket URL from ws to wss

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ const [chat, setChat] = useState<MessageProps[]>([]);
 
   useEffect(() => {
     //Django ka Websocket URL(Jo routing.py mein set kiya tha)
-    const ws = new WebSocket("ws://realtime-chat-backend-2awa.onrender.com/ws/chat/");
+    const ws = new WebSocket("wss://realtime-chat-backend-2awa.onrender.com/ws/chat/");
 
     ws.onopen = () => console.log("Django se connect ho gaya!");
 
